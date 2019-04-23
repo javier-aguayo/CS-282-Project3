d497e5d1ce8e89169db23d1b622266766a34cc5e
Nick Warfield <warfield9000@gmail.com>
2019-04-23 10:33:56 -0700

    Worked on Stats Class Done the ground work for the stats class. It must be passed the key file during construction. It then takes an input file to read and counts it. A print function will print the current contents.

--- Stats.java
+++ Stats.java
@@ -1,4 +1,42 @@
+import java.io.File;
+import java.util.Vector;
+
 public class Stats
 {
+    private class Entry
+    {
+        public Entry(String k)
+        {
+            key = k;
+            count = 0;
+        }
 
+        public String Key() { return key; }
+        public int Count() { return count; }
+        public void Plus() { count++; }
+        public void Reset() { count = 0; }
+
+        private String key;
+        private int count;
+    }
+
+    private int lineCount = 0, wordCount = 0;
+    private Vector<Entry> words;
+    private Hash hash;
+
+    public Stats(File keys)
+    {
+        
+    }
+
+    public void Read(File text)
+    {
+
+    }
+
+    public void Print()
+    {
+
+    }
 }
+
